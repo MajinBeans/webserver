30ef0e83f21bbfd127651147c056e68eee0f9aee
MajinBeans <dean.baeten@gmail.com>
2023-06-21 13:08:34 -0400

    added remove task

--- requesthandler.py
+++ requesthandler.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import cgi ###common gateway interface###
+import cgi 
+#common gateway interface#
 
 tasklist = ['Task 1', 'Task 2', 'Task 3']
 
@@ -16,6 +17,7 @@ class requestHandler(BaseHTTPRequestHandler):
             output += '<h3><a href="/tasklist/new">Add New Task</a></h3>'
             for task in tasklist:
                 output += task
+                output += '<a/ href="/tasklist/%s/remove">X</a>' % task
                 output += '<br>'
             
             output += '</body></html>'
@@ -29,7 +31,6 @@ class requestHandler(BaseHTTPRequestHandler):
             output = ''
             output += '<html><body>'
             output += '<h1>Add New Task</h1>'
-
             output += '<form method="POST" enctype="multipart/form-data action="/tasklist/new">'
             output += '<input name="task" type="text" placeholder="Add New Task">'
             output += '<input type="submit" value="Add">'
@@ -37,16 +38,30 @@ class requestHandler(BaseHTTPRequestHandler):
             output += '</body></html>'
 
             self.wfile.write(output.encode())
+
+        if self.path.endswith('/remove'):
+            listIDPath = self.path.split('/')[2]
+            print(listIDPath)
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            
+            output = ''
+            output += '<html><body>'
+            output += '<h1>Remove Task: %s</h1>' % listIDPath.replace('%20', ' ')
+            output += '</body></html>'
+            self.wfile.write(output.encode())
+
     def do_POST(self):
         if self.path.endswith('/new'):
             ctype, pdict = cgi.parse_header(self.headers.get('content-type')) ###ctype scans POST and = multipart/form-data | pdict will be Boundary Key from form-data to separate values in the form###
-            content_len = int(self.headers.get('Content-Length'))
             pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            content_len = int(self.headers.get('Content-length'))
             pdict['CONTENT-LENGTH'] = content_len
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 new_task = fields.get('task')
-                tasklist.append(new_task)
+                tasklist.append(new_task[0])
 
             self.send_response(301) ###redirect request###
             self.send_header('content-type', 'text/html')
